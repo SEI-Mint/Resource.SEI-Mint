3f3b082b148fe04eb1cc0d0bdb22294733f391c4
mint_cai <goodjoynice@gmail.com>
2024-04-09 16:07:00 +0800

    Update JS/Custom.js

--- JS/Custom.js
+++ JS/Custom.js
@@ -1,6 +1,24 @@
 (function(){
+/***************************************************************************************************
+ ===================================================================================================
+	Declarations and Definitions
+ ===================================================================================================
+ **************************************************************************************************/
+    var URL     = "https://raw.githubusercontent.com/SEI-Mint/Resource.SEI-Mint/main/";
+
+    var URN     = {
+        "Custom" : "JS/Custom.js",
+        "Menu"   : "JS/Menu.js"
+    };
+
+    var Include = {};
     var Disable = {};
-    
+
+/***************************************************************************************************
+ ===================================================================================================
+	Function [ Disable ]
+ ===================================================================================================
+ **************************************************************************************************/
     Disable.DeveloperTools = {};
     Disable.DeveloperTools.Event = function(e)
     {
@@ -43,4 +61,33 @@
     };
     Disable.DeveloperTools.Init();
 
+/***************************************************************************************************
+ ===================================================================================================
+	Function [ Include ]
+ ===================================================================================================
+ **************************************************************************************************/
+    Include.Response = function(e)
+    {
+        var Target = e.target || e.srcElement || e.currentTarget;
+        if(Target.readyState == 4){
+            let Element_Script = document.createElement("script");
+            Element_Script.innerText = Target.responseText;
+            document.body.append(Element_Script);
+        }
+    };
+
+    Include.Request  = function(Item) {
+        if ( typeof URN[Item] != "undefined" ) {
+            let URI  = URL + URN[Item];
+            let HTTP = new XMLHttpRequest();
+            let Func = function(Action, Index, List) {
+                HTTP.addEventListener(Action, Include.Response);
+            };
+            HTTP.open("GET", URI, true);
+            ["progress", "load", "error", "abort"].forEach(Func);
+            HTTP.send();
+        }
+    };
+
+    Include.Request("Menu");
 })();
