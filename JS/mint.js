90051d2a527bc99a8954f4440d898b546e3dfb4f
Mint <40682150+SEI-Mint@users.noreply.github.com>
2025-12-16 22:04:25 +0800

    Update mint.js

--- JS/mint.js
+++ JS/mint.js
@@ -1,3 +1,51 @@
-(function (...args) {
-    console.log(...args);
-})(window, document, M);
+(function (window, document, proto) {
+    if (!proto) return;
+
+    proto.DeveloperTools = {
+        action: {
+            contextmenu: function (e) {
+                if (!e) return;
+                e.preventDefault();
+                e.stopPropagation();
+            },
+            keydown: function (e) {
+                if (!e) return;
+                const keycode = e.which ?? e.keyCode ?? 0;
+                switch (keycode) {
+                    case 67:
+                    case 73:
+                        if (e?.shiftKey === true && e?.ctrlKey === true) {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            alert(_("DeveloperTools Disabled"));
+                        }
+                        break;
+                    case 123:
+                        e.preventDefault();
+                        e.stopPropagation();
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        },
+        event: function (e) {
+            const self = proto.DeveloperTools;
+            const action = e?.type ?? "unknown";
+            if (action in self.action) {
+                self.action[action](e);
+            }
+        },
+        disable: function (e) {
+            const self = proto.DeveloperTools;
+            for (let action in self.action) {
+                document.body.addEventListener(action, proto.DeveloperTools.event);
+            }
+        },
+    }
+
+    if (M) {
+        M.DeveloperTools.disable();
+    }
+})(window, document, M?.constructor?.prototype);
